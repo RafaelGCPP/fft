562f358c5b5c1fb07d1dcec414ff6607f27529d7
Rafael Pinto <rafaelgcpp@gmail.com>
2024-01-15 20:37:25 -0300

    alternative twiddle factor calculation

--- fft_init.c
+++ fft_init.c
@@ -3,11 +3,31 @@
 
 void precompute_twiddle_factors(float *twiddle, int n)
 {
-    // float complex *twd = (float complex *)twiddle;
     for (int j = 0; j < n / 2; j++)
     {
         twiddle[2 * j] = cosf(-2.0 * M_PI * j / n);
         twiddle[2 * j + 1] = sinf(-2.0 * M_PI * j / n);
+    }
+}
+
+void precompute_twiddle_factors_sqrt(float *twiddle, int n)
+{
+    twiddle[0] = 1.0; // 0 rad - i=0
+    twiddle[1] = 0.0;
+    twiddle[n / 2] = 0.0; // -pi/2 rad - i=n/4
+    twiddle[n / 2 + 1] = -1.0;
+
+    for (int m = n / 4; m > 1; m >>= 1)
+    {
+        int j = m / 2;
+        twiddle[2 * j] = sqrtf((1.0 + twiddle[2 * (2 * j)]) / 2.0);
+        twiddle[2 * j + 1] = -sqrtf((1.0 - twiddle[2 * (2 * j)]) / 2.0);
+
+        for (int i = m + j; i < n / 2; i += j)
+        {
+            twiddle[2 * i] = twiddle[2 * (i - j)] * twiddle[m] - twiddle[2 * (i - j) + 1] * twiddle[m + 1];
+            twiddle[2 * i + 1] = twiddle[2 * (i - j)] * twiddle[m + 1] + twiddle[2 * (i - j) + 1] * twiddle[m];
+        }
     }
 }
 
--- main.c
+++ main.c
@@ -5,12 +5,13 @@
 
 int main()
 {
-    int n = 8;                                                                                               // Change this value for different sizes of the arrays (N)
+    int n = 16;                                                                                              // Change this value for different sizes of the arrays (N)
     float data[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0}; // interleaved real and imaginary parts
     float twiddle[n];
     int bitrev[n];
 
     // Precompute twiddle factors
+    n = 8;
     precompute_twiddle_factors(twiddle, n);
     precompute_bitrev_table(bitrev, n);
 
@@ -34,26 +35,24 @@ int main()
         printf("%.5f + %.5fi\n", data[2 * i], data[2 * i + 1]);
     }
 
-
-    float twiddle2[2*n];
+    n = 16;
 
     // Precompute twiddle factors
-    precompute_twiddle_factors(twiddle2, 2*n);
+    precompute_twiddle_factors(twiddle, n);
 
-    rfft(data, n * 2, twiddle2, bitrev);
+    rfft(data, n, twiddle, bitrev);
     printf("FFT result:\n");
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n / 2; i++)
     {
         printf("%.5f + %.5fi\n", data[2 * i], data[2 * i + 1]);
     }
 
-    irfft(data, n * 2, twiddle2, bitrev);
+    irfft(data, n, twiddle, bitrev);
     printf("IFFT result:\n");
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n / 2; i++)
     {
         printf("%.5f + %.5fi\n", data[2 * i], data[2 * i + 1]);
     }
-
 
     return 0;
 }
