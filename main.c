45099639ee362d43d7e44a147ddb006d97f62461
Rafael Pinto <rafaelgcpp@gmail.com>
2024-01-20 07:32:49 -0300

    activating all tests

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 int main()
 {
 
-    // cordic_test();
-    // twiddle_test();
-    // fix_twiddle_test();
+    cordic_test();
+    twiddle_test();
+    fix_twiddle_test();
     complex_fft_test();
     real_fft_test();
-    // complex_fft_test_fix();
+    complex_fft_test_fix();
     real_fft_test_fix();
 
     return 0;
